642edf66e352dd8e37d9c2248f9f30493a9c9a7f
kdany25 <kabadany25@gmail.com>
2023-02-22 12:48:32 +0200

    feat: added Home header

--- navigators/RootStack.tsx
+++ navigators/RootStack.tsx
@@ -1,26 +1,83 @@
-import React, {FunctionComponent} from "react"
+import React, { FunctionComponent } from "react";
 
 //React Navigation
-import {NavigationContainer} from "@react-navigation/native"
-import {createStackNavigator} from "@react-navigation/stack"
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 //Screens
-import Welcome from "./../screens/Welcome"
+import Welcome from "./../screens/Welcome";
+import Home from "../screens/Home";
+
+//colors
+import { colors } from "../components/colors";
+
+//customer component
+import Greeting from "../components/Header/Greeting";
+import Profile from "../components/Header/Profile";
+
+//Image
+import Avi from "./../assets/avi/avatar.png";
 
 export type RootStackParamList = {
-  Welcome: undefined
-}
+	Welcome: undefined;
+	Home: undefined;
+};
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-const RootStack: FunctionComponent =() => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Welcome} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-} 
+const RootStack: FunctionComponent = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: colors.graylight,
+						borderBottomWidth: 0,
+						shadowOpacity: 0,
+						elevation: 0,
+						height: 120,
+					},
+					headerTintColor: colors.secondary,
+					headerRightContainerStyle: {
+						paddingRight: 25,
+					},
+					headerLeftContainerStyle: {
+						paddingLeft: 10,
+					},
+					headerRight: () => (
+						<Profile
+							img={Avi}
+							imgContainerStyle={{
+								backgroundColor: colors.tertiary,
+							}}
+						/>
+					),
+				}}
+				initialRouteName="Home"
+			>
+				<Stack.Screen
+					name="Welcome"
+					component={Welcome}
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen
+					name="Home"
+					component={Home}
+					options={{
+						headerTitle: (props) => (
+							<Greeting
+								mainText="Hey coby!"
+								subText="Welcome back"
+								{...props}
+							/>
+						),
 
-export default RootStack
+						headerLeft: () => <></>,
+					}}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default RootStack;
---
+++ screens/Home.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+import { StatusBar } from "expo-status-bar";
+import styled from "styled-components/native";
+
+const Home: FunctionComponent = () => {
+	return <></>;
+};
+export default Home;
